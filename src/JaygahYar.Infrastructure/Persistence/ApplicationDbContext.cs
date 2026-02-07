5670990b1db79ad9f20f2cf426023dfae2fbd06b
Maryam Rezaei <Rezaie.m@entekhabelectronic.com>
2026-02-07 09:02:07 +0330

    Add dbcontext and migration

--- src/JaygahYar.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/JaygahYar.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -21,6 +21,7 @@ public class ApplicationDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+        modelBuilder.HasDefaultSchema("JaygahYar");
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
 
         modelBuilder.Entity<Station>(e =>
